ccc2897c873ecc56c3209454dff947de73845077
Alexis-Marion <alexis.marion@umontpellier.fr>
2024-01-19 13:29:08 +0100

    Update geo_parser.sh

--- geo_parser.sh
+++ geo_parser.sh
@@ -1,3 +1,6 @@
+### Author Alexis MARION
+
+
 while read line; do
   Genus=$(echo "$line" |cut -d"," -f1| awk '{print $1}')
   Species=$(echo "$line" |cut -d"," -f2|sed -e 's:^M::g'|sed -e 's:\r::g'| awk '{print $2}')
@@ -9,6 +12,7 @@ while read line; do
   outfile=$(echo "$Genus")
   outfile+=$(echo "_")
   outfile+=$(echo "$Species")
+  outfile+=$(echo ".kml")
   outfile2=$(echo "$Genus")
   outfile2+=$(echo "_")
   outfile2+=$(echo "$Species")
@@ -16,5 +20,6 @@ while read line; do
   wget -O $outfile $arg
   echo "$Genus,$Species" >> $outfile2
   cat   $outfile| grep -i "<Point><coordinates>"|sed -e 's:<Point><coordinates>::g'|sed -e 's:</coordinates></Point>::g'|sed -e 's:\t::g' >> $outfile2
+  rm *.kml
 
 done < Species_list.txt
